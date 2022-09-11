cb81ccdb95bbc51605a65a548e98b4e617f4de32
geshinade <106628927+geshinade@users.noreply.github.com>
2022-09-11 17:21:41 +0100

    vendor and customer dashboard edits

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,7 @@ from .utils import detectUser, send_verification_email
 from django.contrib.auth.decorators import login_required, user_passes_test
 
 from django.core.exceptions import PermissionDenied
+from vendor.models import Vendor
 
 # Create your views here.
 
@@ -167,7 +168,8 @@ def custDashboard(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
-   return render(request, 'accounts/vendorDashboard.html')
+   
+    return render(request, 'accounts/vendorDashboard.html')
 
 def forgot_password(request):
     if request.method == 'POST':
