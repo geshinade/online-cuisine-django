76b7cf2e9a1e2eac43c0ed43abca3becc8ce8ca5
geshinade <106628927+geshinade@users.noreply.github.com>
2022-09-20 09:38:49 +0100

    customer dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,7 @@ from django.core.exceptions import PermissionDenied
 from vendor.models import Vendor
 
 from django.template.defaultfilters import slugify
+from orders.models import Order
 
 # Create your views here.
 
@@ -166,7 +167,14 @@ def myAccount(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_customer)
 def custDashboard(request):
-   return render(request, 'accounts/custDashboard.html')
+    orders = Order.objects.filter(user=request.user, is_ordered=True)
+    recent_orders = orders[:5]
+    context = {
+        'orders': orders,
+        'orders_count': orders.count(),
+        'recent_orders': recent_orders,
+    }
+    return render(request, 'accounts/custDashboard.html', context)
 
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
